fda351dd62235af26a9fefcf50a94b84edfdebe5
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-10-14 08:56:11 +0530

    Pushing code to remote , simple msg to user created in FE

---
+++ frontEnd/src/App.tsx
@@ -0,0 +1,21 @@
+
+import "./index.css"
+
+function App() {
+
+  return (
+    <>
+      <div style={ { height : "100vh" } } className=" bg-sky-300 text-center flex flex-col justify-center items-center text-white">
+
+        <h1 className=" text-3xl font-bold underline animate__animated animate__flip animate__slower animate__infinite ">
+          I'll build this web app ASP
+        </h1>
+        <p className="text-red-500 font-bold mt-4">Stay connected 😊</p>
+        <a className=" text-violet-400 font-bold underline text-sm" href="https://ashish-kuldeep-portfolio.vercel.app/" target="_blank">See my portfolio web</a>
+
+      </div>
+    </>
+  )
+}
+
+export default App
