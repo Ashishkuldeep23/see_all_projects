fb331b534c99e55e5009f5e37d81210736fdb2f5
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-10-14 12:45:23 +0530

    Error msg added

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -5,13 +5,14 @@ function App() {
 
   return (
     <>
-      <div style={ { height : "100vh" } } className=" bg-sky-300 text-center flex flex-col justify-center items-center text-white">
+      <div style={ { height : "100vh" } } className=" bg-sky-300 text-center flex flex-col items-center text-white">
 
-        <h1 className=" text-3xl font-bold underline animate__animated animate__flip animate__slower animate__delay-1s animate__infinite ">
+        <h1 className=" pt-28 text-3xl font-bold underline animate__animated animate__flip animate__slower animate__delay-1s animate__infinite ">
           I'll build this web app ASP
         </h1>
         <p className="text-red-500 font-bold mt-4">Stay connected 😊</p>
-        <a className=" text-violet-400 font-bold underline text-sm" href="https://ashish-kuldeep-portfolio.vercel.app/" target="_blank">See my portfolio web</a>
+        <a className=" text-violet-400 font-bold underline text-sm" href="https://ashish-kuldeep-portfolio.vercel.app/" target="_blank">See my portfolio web 🔗</a>
+        <p>I'll show all coding work here (Idea is that) </p>
 
       </div>
     </>
